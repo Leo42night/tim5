b167ff75f8e7675406787bf417b87e69a31874a2
Blackpa77 <h1101241029@student.untan.ac.id>
2025-11-26 15:22:35 +0700

    tes

--- config/database.php
+++ config/database.php
@@ -19,4 +19,16 @@ class Database {
         return $this->conn;
     }
 }
+
+<?php
+// Railway menyediakan MySQL variables otomatis
+return [
+    'host' => getenv('MYSQLHOST') ?: 'localhost',
+    'port' => getenv('MYSQLPORT') ?: '3306',
+    'dbname' => getenv('MYSQLDATABASE') ?: 'mvc_db',
+    'username' => getenv('MYSQLUSER') ?: 'root',
+    'password' => getenv('MYSQLPASSWORD') ?: '',
+    'charset' => 'utf8mb4'
+];
+
 ?>
